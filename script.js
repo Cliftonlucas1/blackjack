343b85164e4578745c672fc4b859ba81e069c7a0
Clifton Lucas <cliftonlucas89@gmail.com>
2023-01-25 10:39:01 -0600

    buttons are functional

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 let cards = []
-let DealerCard = []
+let dealerCard = []
 let sum = 0
+let dealerSum
 let win = false
 const startGameButton = document.querySelector('.start-Game')
 const newCardButton = document.querySelector('.new-Card')
 const player = document.querySelector('.player-l')
 const total = document.querySelector('.sum-l')
 const message = document.querySelector('.message-l')
+const resetButton = document.querySelector('.reset-button')
+const stayButton = document.querySelector('.stay-button')
 
 
 
 
 let deck = [
-'2-C', '3-C', '4-C', '5-C', '6-C', '7-C', '8-C', '9-C', '10-C', '10-CJ', '10-CQ', '10-CK', '11-CA',                
+  
+'2-C', '3-C', '4-C', '5-C', '6-C', '7-C', '8-C', '9-C', '10-C', '10-CJ', '10-CQ', '10-CK', '11-CA',      
+
 '2-D', '3-D', '4-D', '5-D', '6-D', '7-D', '8-D', '9-D', '10-D', '10-DJ', '10-DQ', '10-DK', '11-DA',
+
 '2-H', '3-H', '4-H', '5-H', '6-H', '7-H', '8-H', '9-H', '10-H', '10-HJ', '10-HQ', '10-HK', '11-HA',
+
 '2-S', '3-S', '4-S', '5-S', '6-S', '7-S', '8-S', '9-S', '10-S', '10-SJ', '10-SQ', '10-SK', '11-SA'
+
 ]
 
 
@@ -282,19 +290,84 @@ let deck = [
 
 // }
 
+const checkDealerWin = () => {
+  if (dealerSum === 21) {
+   message.innerHTML = "Dealer have BlackJack!"
+   console.log("Dealer have BlackJack!")
+   } else if(dealerSum >= 22){
+   message.innerHTML = "You have Won BlackJack!"
+   console.log("You have Won BlackJack!")
+   } else if(dealerSum > sum && dealerSum < 22){
+     message.innerHTML = "Dealer have BlackJack!"
+   console.log("Dealer have BlackJack!")
+   } else if (sum > dealerSum && sum < 22){
+   message.innerHTML = "You have Won BlackJack!"
+    console.log("You have Won BlackJack!")
+   }
+ }
+
+const stayFunction = () => {
+startGameButton.disabled = true
+stayButton.disabled = true
+newCardButton.disabled = true
+dealerFunction()
+}
+
+
+
+const dealerFunction = () => {
+  let x = deck[0]
+  let finalValue = []
+  dealerCard.push(x)
+  dealerSum += dealerCard
+  
+dealerCard.forEach((num) =>{
+finalValue.push(parseInt(num))
+console.log(finalValue)
+    // return finalValue
+    
+  })
+ dealerSum = finalValue.reduce((int, value) => int + value, 0)
+
+ if(dealerSum < 17){
+   dealerFunction()
+ } else {
+  checkDealerWin()
+  }
+ 
+  
+ console.log("Dealer draw" + " " + dealerSum)
+ console.log("Your draw" + " " + sum)
+}
+
+
+
+
+
+
+stayButton.addEventListener('click', () => stayFunction())
+
+resetButton.addEventListener('click', () => resetGame())
+
+
+const resetGame = () => {
+location.reload()
+
+
+ }
 
 const startGame = () => {
-// player.innerHTML = "Cards: " + " " + firstCard
-// total.innerHTML = "Sum: " + " " + sum
 deck.sort((a, b) => 0.5 - Math.random())
 
 let x = deck[0]
 cards.push(x)
 
 deck.shift()
-
-
+player.innerHTML = "Cards: " + " " + cards
+total.innerHTML = "Sum: " + " " + cards
+startGameButton.disabled = true
 checkWin()
+
 console.log(parseInt(cards))
   console.log("you have pressed the button")
  
@@ -306,14 +379,19 @@ const newCard = () => {
   let finalValue = []
   cards.push(x)
   sum =+ cards
-  
+  deck.shift()
   cards.forEach((num) =>{
 finalValue.push(parseInt(num))
-// console.log(finalValue)
-    return finalValue
+sum = finalValue.reduce((int, value) => int + value, 0)
     
+player.innerHTML = "Cards: " + " " + cards
+total.innerHTML = "Sum: " + " " + sum
+
+    return finalValue
+
+
   })
- sum = finalValue.reduce((int, value) => int + value, 0)
+
 console.log(sum)
 
 checkWin()
@@ -326,17 +404,26 @@ newCardButton.addEventListener('click', () => newCard())
 
 const checkWin = () => {
   if(sum < 20){
-  //  message.innerHTML = "Draw A New Card"
-   console.log("Draw A New Card")
-    win = false;
+  message.innerHTML = "Do You Want To Draw A New Card?"
+   console.log("Do You Want To Draw A New Card?")
   } else if (sum === 21){
+    message.innerHTML = "You have BlackJack!"
     console.log("You have BlackJack!")
-    win = true;
+    startGameButton.disabled = true
+    stayButton.disabled = true
+    newCardButton.disabled = true
+    
   } else if (sum > 21) {
+    message.innerHTML = 'You have lost the game!'
     console.log("You have lost the game!")
-    win = false;
+    startGameButton.disabled = true
+    stayButton.disabled = true
+    newCardButton.disabled = true
   }
+
+
   
+
 }
 
 
