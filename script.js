35397f85c1e9fef621eab8f519dae0dfdd83f242
Clifton Lucas <cliftonlucas89@gmail.com>
2023-01-25 19:42:24 -0600

    deck object bugs fixed

--- script.js
+++ script.js
@@ -14,285 +14,407 @@ const stayButton = document.querySelector('.stay-button')
 
 
 
-let deck = [
-  
-'2-C', '3-C', '4-C', '5-C', '6-C', '7-C', '8-C', '9-C', '10-C', '10-CJ', '10-CQ', '10-CK', '11-CA',      
+// let deck = [
 
-'2-D', '3-D', '4-D', '5-D', '6-D', '7-D', '8-D', '9-D', '10-D', '10-DJ', '10-DQ', '10-DK', '11-DA',
+// '2-C', '3-C', '4-C', '5-C', '6-C', '7-C', '8-C', '9-C', '10-C', '10-CJ', '10-CQ', '10-CK', '11-CA',      
 
-'2-H', '3-H', '4-H', '5-H', '6-H', '7-H', '8-H', '9-H', '10-H', '10-HJ', '10-HQ', '10-HK', '11-HA',
+// '2-D', '3-D', '4-D', '5-D', '6-D', '7-D', '8-D', '9-D', '10-D', '10-DJ', '10-DQ', '10-DK', '11-DA',
 
-'2-S', '3-S', '4-S', '5-S', '6-S', '7-S', '8-S', '9-S', '10-S', '10-SJ', '10-SQ', '10-SK', '11-SA'
+// '2-H', '3-H', '4-H', '5-H', '6-H', '7-H', '8-H', '9-H', '10-H', '10-HJ', '10-HQ', '10-HK', '11-HA',
 
-]
+// '2-S', '3-S', '4-S', '5-S', '6-S', '7-S', '8-S', '9-S', '10-S', '10-SJ', '10-SQ', '10-SK', '11-SA'
 
-
-
-//  let deck = {
-
-// spadeTwo: {
-//   name: 'Spade of Two',
-//   value: 2
-// },
-// spadeThree: {
-//   name: 'Spade of Three',
-//   value: 3
-// },
-// spadeFour: {
-//   name: 'Spade of Four',
-//   value: 4
-// },
-
-// spadeFive: {
-//   name: 'Spade of Five',
-//   value: 5
-// },
-
-// spadeSix: {
-//   name: 'Spade of Six',
-//   value: 6
-// },
-// spadeSeven: {
-//   name: 'Spade of Seven',
-//   value: 7
-// },
-// spadeEight: {
-//   name: 'Spade of Eight',
-//   value: 8
-// },
-
-// spadeNine: {
-//   name: 'Spade of Nine',
-//   value: 9
-// },
-
-// spadeTen: {
-//   name: 'Spade of Ten',
-//   value: 10
-// },
-
-// spadeKing: {
-//   name: 'Ace of King',
-//   value: 10
-// },
-// spadeQueen: {
-//   name: 'Spade of Queen',
-//   value: 10
-// },
-
-// spadeJack: {
-//   name: 'Spade of Club',
-//   value: 10
-// },
-
-
-// spadeAce: {
-//   name: 'Spade of Ace',
-//   value: 11
-// },
+// ]
 
 
 
+  let deck = [
 
 
+{
+  name: 'Two',
+  suit: "Diamond",
+  value: 2,
+  img: " "
+},
 
+{
+  name: 'Three',
+  suit: "Diamond",
+  value: 3,
+  img: " "
+},
 
-// heartTwo: {
-//   name: 'Heart of Two',
-//   value: 2
-// },
-// heartThree: {
-//   name: 'Heart of Three',
-//   value: 3
-// },
-// heartFour: {
-//   name: 'Heart of Four',
-//   value: 4
-// },
+{
+  name: 'Four',
+  suit: "Diamond",
+  value: 4,
+  img: " "
+},
 
-// heartFive: {
-//   name: 'Heart of Five',
-//   value: 5
-// },
+{
+    name: 'Five',
+    suit: "Diamond",
+    value: 5,
+    img: " "
+},
 
-// heartSix: {
-//   name: 'Heart of Six',
-//   value: 6
-// },
-// heartSeven: {
-//   name: 'Heart of Seven',
-//   value: 7
-// },
-// heartEight: {
-//   name: 'Heart of Eight',
-//   value: 8
-// },
+{
+    name: 'Six',
+    suit: "Diamond",
+    value: 6,
+    img: " "
+},
 
-// heartNine: {
-//   name: 'Heart of Nine',
-//   value: 9
-// },
+{
+    name: 'Seven',
+    suit: "Diamond",
+    value: 7,
+    img: " "
+},
 
-// heartTen: {
-//   name: 'Heart of Ten',
-//   value: 10
-// },
+{
+    name: 'Eight',
+    suit: "Diamond",
+    value: 8,
+    img: " "
+},
 
-// HeartKing: {
-//   name: 'Heart of King',
-//   value: 10
-// },
-// heartQueen: {
-//   name: 'Heart of Queen',
-//   value: 10
-// },
+{
+    name: 'Nine',
+    suit: "Diamond",
+    value: 9,
+    img: " "
+},
 
-// heartJack: {
-//   name: 'Heart of Club',
-//   value: 10
-// },
+{
+    name: 'Ten',
+    suit: "Diamond",
+    value: 10,
+    img: " "
+},
 
-// heartAce: {
-//   name: 'Heart of Ace',
-//   value: 11
-// },
+{
+    name: 'Jack',
+    suit: "Diamond",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'Queen',
+    suit: "Diamond",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'King',
+    suit: "Diamond",
+    value: 10,
+    img: " "
+},
+
+{
+  name: 'Spade',
+  suit: "Diamond",
+  value: 11,
+  img: " "
+},
 
 
 
 
+{
+  name: 'Two',
+  suit: "Heart",
+  value: 2,
+  img: " "
+},
+
+{
+  name: 'Three',
+  suit: "Heart",
+  value: 3,
+  img: " "
+},
+
+{
+  name: 'Four',
+  suit: "Heart",
+  value: 4,
+  img: " "
+},
+
+{
+    name: 'Five',
+    suit: "Heart",
+    value: 5,
+    img: " "
+},
+
+{
+    name: 'Six',
+    suit: "Heart",
+    value: 6,
+    img: " "
+},
+
+{
+     
+    name: 'Seven',
+    suit: "Heart",
+    value: 7,
+    img: " "
+},
+
+{
+    name: 'Eight',
+    suit: "Heart",
+    value: 8,
+    img: " "
+},
+
+{
+    name: 'Nine',
+    suit: "Heart",
+    value: 9,
+    img: " "
+},
+
+{
+    name: 'Ten',
+    suit: "Heart",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'Jack',
+    suit: "Heart",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'Queen',
+    suit: "Heart",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'King',
+    suit: "Heart",
+    value: 10,
+    img: " "
+},
+
+{
+  name: 'Spade',
+  suit: "Heart",
+  value: 11,
+  img: " "
+},
 
 
 
-// clubTwo: {
-//   name: 'Club of Two',
-//   value: 2
-// },
-// clubThree: {
-//   name: 'Club of Three',
-//   value: 3
-// },
-// clubFour: {
-//   name: 'Club of Four',
-//   value: 4
-// },
-
-// clubFive: {
-//   name: 'Club of Five',
-//   value: 5
-// },
-
-// clubSix: {
-//   name: 'Club of Six',
-//   value: 6
-// },
-// clubSeven: {
-//   name: 'Club of Seven',
-//   value: 7
-// },
-// clubEight: {
-//   name: 'Club of Eight',
-//   value: 8
-// },
-
-// clubNine: {
-//   name: 'Club of Nine',
-//   value: 9
-// },
-
-// clubTen: {
-//   name: 'Club of Ten',
-//   value: 10
-// },
-
-// clubKing: {
-//   name: 'Club of King',
-//   value: 10
-// },
-// clubQueen: {
-//   name: 'club of Queen',
-//   value: 10
-// },
-
-// clubJack: {
-//   name: 'Club of Club',
-//   value: 10
-// },
-
-// clubAce: {
-//   name: 'Club of Ace',
-//   value: 11
-// },
 
 
 
+{
+  name: 'Two',
+  suit: "Club",
+  value: 2,
+  img: " "
+},
 
-// diamondTwo: {
-//   name: 'Diamond of Two',
-//   value: 2
-// },
-// diamondThree: {
-//   name: 'Diamond of Three',
-//   value: 3
-// },
-// diamondFour: {
-//   name: 'Diamond of Four',
-//   value: 4
-// },
+{
+  name: 'Three',
+  suit: "Club",
+  value: 3,
+  img: " "
+},
 
-// diamondFive: {
-//   name: 'Diamond of Five',
-//   value: 5
-// },
+{
+  name: 'Four',
+  suit: "Club",
+  value: 4,
+  img: " "
+},
 
-// diamondSix: {
-//   name: 'Diamond of Six',
-//   value: 6
-// },
-// diamondSeven: {
-//   name: 'Diamond of Seven',
-//   value: 7
-// },
-// diamondEight: {
-//   name: 'Diamond of Eight',
-//   value: 8
-// },
+{
+    name: 'Five',
+    suit: "Club",
+    value: 5,
+    img: " "
+},
 
-// diamondNine: {
-//   name: 'Diamond of Nine',
-//   value: 9
-// },
+{
+    name: 'Six',
+    suit: "Club",
+    value: 6,
+    img: " "
+},
 
-// diamondTen: {
-//   name: 'Diamond of Ten',
-//   value: 10
-// },
+{  
+    name: 'Seven',
+    suit: "Club",
+    value: 7,
+    img: " "
+},
 
-// diamondKing: {
-//   name: 'Diamond of King',
-//   value: 10
-// },
-// diamondQueen: {
-//   name: 'Diamond of Queen',
-//   value: 10
-// },
+{
+    name: 'Eight',
+    suit: "Club",
+    value: 8,
+    img: " "
+},
 
-// diamondJack: {
-//   name: 'Diamond of Club',
-//   value: 10
-// },
+{
+    name: 'Nine',
+    suit: "Club",
+    value: 9,
+    img: " "
+},
 
-// diamondAce: {
-//   name: 'Diamond of Ace',
-//   value: 11
-// },
+{
+    name: 'Ten',
+    suit: "Club",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'Jack',
+    suit: "Club",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'Queen',
+    suit: "Club",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'King',
+    suit: "Club",
+    value: 10,
+    img: " "
+},
+
+{
+  name: 'Spade',
+  suit: "Club",
+  value: 11,
+  img: " "
+},
 
 
 
-// }
+{
+  name: 'Two',
+  suit: "Ace",
+  value: 2,
+  img: " "
+},
+
+{
+  name: 'Three',
+  suit: "Ace",
+  value: 3,
+  img: " "
+},
+
+{
+  name: 'Four',
+  suit: "Ace",
+  value: 4,
+  img: " "
+},
+
+{
+    name: 'Five',
+    suit: "Ace",
+    value: 5,
+    img: " "
+},
+
+{
+    name: 'Six',
+    suit: "Ace",
+    value: 6,
+    img: " "
+},
+
+{    
+    name: 'Seven',
+    suit: "Ace",
+    value: 7,
+    img: " "
+},
+
+{
+    name: 'Eight',
+    suit: "Ace",
+    value: 8,
+    img: " "
+},
+
+{
+    name: 'Nine',
+    suit: "Ace",
+    value: 9,
+    img: " "
+},
+
+{
+    name: 'Ten',
+    suit: "Ace",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'Jack',
+    suit: "Ace",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'Queen',
+    suit: "Ace",
+    value: 10,
+    img: " "
+},
+
+{
+    name: 'King',
+    suit: "Ace",
+    value: 10,
+    img: " "
+},
+
+{ 
+  name: 'Spade',
+  suit: "Ace",
+  value: 11,
+  img: " "
+}
+
+  ]
+
+
+
+
 
 const checkDealerWin = () => {
   if (dealerSum === 21) {
-   message.innerHTML = "Dealer have BlackJack!"
+   message.innerHTML = "Dealer have BlackJack!" + "Dealer Sum: " + dealerSum + "Player Sum: " + sum
    console.log("Dealer have BlackJack!")
    } else if(dealerSum >= 22){
    message.innerHTML = "You have Won BlackJack!"
@@ -316,18 +438,21 @@ dealerFunction()
 
 
 const dealerFunction = () => {
-  let x = deck[0]
-  let finalValue = []
+  const randomIndex = (Math.floor(Math.random()*51))
+  let x = deck[randomIndex].value
+  console.log(x)
   dealerCard.push(x)
+  let finalValue = []
   dealerSum += dealerCard
+  deck.shift()
   
-dealerCard.forEach((num) =>{
-finalValue.push(parseInt(num))
-console.log(finalValue)
-    // return finalValue
+ dealerCard.forEach((num) =>{
+ finalValue.push(num)
+ console.log(finalValue)
+ dealerSum = finalValue.reduce((int, value) => int + value, 0)
     
   })
- dealerSum = finalValue.reduce((int, value) => int + value, 0)
+
 
  if(dealerSum < 17){
    dealerFunction()
@@ -357,31 +482,38 @@ location.reload()
  }
 
 const startGame = () => {
-deck.sort((a, b) => 0.5 - Math.random())
+const randomIndex = (Math.floor(Math.random()*51))
 
-let x = deck[0]
+
+
+let x = deck[randomIndex].value
+// let x = deck[0]
 cards.push(x)
 
 deck.shift()
+console.log(x)
+startGameButton.disabled = true
 player.innerHTML = "Cards: " + " " + cards
 total.innerHTML = "Sum: " + " " + cards
-startGameButton.disabled = true
-checkWin()
 
-console.log(parseInt(cards))
+// checkWin()
+
+
   console.log("you have pressed the button")
  
 }
 
 
 const newCard = () => {
-  let x = deck[0]
+  const randomIndex = (Math.floor(Math.random()*51))
+
+  let x = deck[randomIndex].value
   let finalValue = []
   cards.push(x)
   sum =+ cards
   deck.shift()
   cards.forEach((num) =>{
-finalValue.push(parseInt(num))
+finalValue.push(num)
 sum = finalValue.reduce((int, value) => int + value, 0)
     
 player.innerHTML = "Cards: " + " " + cards
