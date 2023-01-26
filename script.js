23e1de6b64ac687281cebc60927f54f03203726d
Clifton Lucas <cliftonlucas89@gmail.com>
2023-01-26 11:00:40 -0600

    recent changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let cards = []
 let dealerCard = []
+
 let sum = 0
 let dealerSum
 let win = false
@@ -10,381 +11,378 @@ const total = document.querySelector('.sum-l')
 const message = document.querySelector('.message-l')
 const resetButton = document.querySelector('.reset-button')
 const stayButton = document.querySelector('.stay-button')
+const compareMessage = document.querySelector('.compare-message')
 
 let deck = [
   {
     name: 'Two',
     suit: 'Diamond',
     value: 2,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r02.svg'
   },
 
   {
     name: 'Three',
     suit: 'Diamond',
     value: 3,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r03.svg'
   },
 
   {
     name: 'Four',
     suit: 'Diamond',
     value: 4,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r04.svg'
   },
 
   {
     name: 'Five',
     suit: 'Diamond',
     value: 5,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r05.svg'
   },
 
   {
     name: 'Six',
     suit: 'Diamond',
     value: 6,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r06.svg'
   },
 
   {
     name: 'Seven',
     suit: 'Diamond',
     value: 7,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r07.svg'
   },
 
   {
     name: 'Eight',
     suit: 'Diamond',
     value: 8,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r08.svg'
   },
 
   {
     name: 'Nine',
     suit: 'Diamond',
     value: 9,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r09.svg'
   },
 
   {
     name: 'Ten',
     suit: 'Diamond',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-r10.svg'
   },
 
   {
     name: 'Jack',
     suit: 'Diamond',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-J.svg'
   },
 
   {
     name: 'Queen',
     suit: 'Diamond',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-Q.svg'
   },
 
   {
     name: 'King',
     suit: 'Diamond',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-K.svg'
   },
 
   {
-    name: 'Spade',
+    name: 'Ace',
     suit: 'Diamond',
     value: 11,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/diamonds/diamonds-A.svg'
   },
 
   {
     name: 'Two',
     suit: 'Heart',
     value: 2,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r02.svg'
   },
 
   {
     name: 'Three',
     suit: 'Heart',
     value: 3,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r03.svg'
   },
 
   {
     name: 'Four',
     suit: 'Heart',
     value: 4,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r04.svg'
   },
 
   {
     name: 'Five',
     suit: 'Heart',
     value: 5,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r05.svg'
   },
 
   {
     name: 'Six',
     suit: 'Heart',
     value: 6,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r06.svg'
   },
 
   {
     name: 'Seven',
     suit: 'Heart',
     value: 7,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r07.svg'
   },
 
   {
     name: 'Eight',
     suit: 'Heart',
     value: 8,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r08.svg'
   },
 
   {
     name: 'Nine',
     suit: 'Heart',
     value: 9,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r09.svg'
   },
 
   {
     name: 'Ten',
     suit: 'Heart',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-r10.svg'
   },
 
   {
     name: 'Jack',
     suit: 'Heart',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-J.svg'
   },
 
   {
     name: 'Queen',
     suit: 'Heart',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-Q.svg'
   },
 
   {
     name: 'King',
     suit: 'Heart',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-K.svg'
   },
 
   {
-    name: 'Spade',
+    name: 'Ace',
     suit: 'Heart',
     value: 11,
-    img: 'images/AC.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/hearts/hearts-A.svg'
   },
 
   {
     name: 'Two',
     suit: 'Club',
     value: 2,
-    img: 'images/2C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r02.svg'
   },
 
   {
     name: 'Three',
     suit: 'Club',
     value: 3,
-    img: 'images/3C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r03.svg'
   },
 
   {
     name: 'Four',
     suit: 'Club',
     value: 4,
-    img: 'images/4C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r04.svg'
   },
 
   {
     name: 'Five',
     suit: 'Club',
     value: 5,
-    img: 'images/5C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r05.svg'
   },
 
   {
     name: 'Six',
     suit: 'Club',
     value: 6,
-    img: 'images/6C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r06.svg'
   },
 
   {
     name: 'Seven',
     suit: 'Club',
     value: 7,
-    img: 'images/7C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r07.svg'
   },
 
   {
     name: 'Eight',
     suit: 'Club',
     value: 8,
-    img: 'images/8C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r08.svg'
   },
 
   {
     name: 'Nine',
     suit: 'Club',
     value: 9,
-    img: 'images/9C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r09.svg'
   },
 
   {
     name: 'Ten',
     suit: 'Club',
     value: 10,
-    img: 'images/10C.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-r10.svg'
   },
 
   {
     name: 'Jack',
     suit: 'Club',
     value: 10,
-    img: 'images/JC.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-J.svg'
   },
 
   {
     name: 'Queen',
     suit: 'Club',
     value: 10,
-    img: 'images/QC.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-Q.svg'
   },
 
   {
     name: 'King',
     suit: 'Club',
     value: 10,
-    img: 'images/KC.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-K.svg'
   },
 
   {
-    name: 'Spade',
+    name: 'Ace',
     suit: 'Club',
     value: 11,
-    img: 'images/AC.png'
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/clubs/clubs-A.svg'
   },
 
   {
     name: 'Two',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 2,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r02.svg'
   },
 
   {
     name: 'Three',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 3,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r03.svg'
   },
 
   {
     name: 'Four',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 4,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r04.svg'
   },
 
   {
     name: 'Five',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 5,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r05.svg'
   },
 
   {
     name: 'Six',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 6,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r06.svg'
   },
 
   {
     name: 'Seven',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 7,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r07.svg'
   },
 
   {
     name: 'Eight',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 8,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r08.svg'
   },
 
   {
     name: 'Nine',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 9,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r09.svg'
   },
 
   {
     name: 'Ten',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-r10.svg'
   },
 
   {
     name: 'Jack',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-J.svg'
   },
 
   {
     name: 'Queen',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-Q.svg'
   },
 
   {
     name: 'King',
-    suit: 'Ace',
+    suit: 'Spade',
     value: 10,
-    img: ' '
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-K.svg'
   },
 
   {
-    name: 'Spade',
-    suit: 'Ace',
+    name: 'Ace',
+    suit: 'Spade',
     value: 11,
-    img: 
+    img: 'https://raw.githubusercontent.com/NobodysLackey/card-deck/a9f7f7f02464f3720273ce00263357ef1c4b5cbf/images/spades/spades-A.svg'
   }
 ]
 
 const checkDealerWin = () => {
   if (dealerSum === 21) {
-    message.innerHTML =
-      'Dealer have BlackJack!' +
-      'Dealer Sum: ' +
-      dealerSum +
-      'Player Sum: ' +
-      sum
+    message.innerHTML = 'Dealer have BlackJack!'
+
     console.log('Dealer have BlackJack!')
   } else if (dealerSum >= 22) {
     message.innerHTML = 'You have Won BlackJack!'
@@ -411,11 +409,13 @@ const dealerFunction = () => {
   dealerCard.push(x)
   let finalValue = []
   dealerSum += dealerCard
-  deck.shift()
+
+  console.log(dealerCard)
 
   dealerCard.forEach((num) => {
     finalValue.push(num)
     dealerSum = finalValue.reduce((int, value) => int + value, 0)
+    deck.shift()
   })
 
   if (dealerSum < 17) {
@@ -423,6 +423,8 @@ const dealerFunction = () => {
   } else {
     checkDealerWin()
   }
+  compareMessage.innerHTML =
+    'Your Sum:' + sum + ' ' + ' ; Dealer Sum:' + dealerSum
 }
 
 stayButton.addEventListener('click', () => stayFunction())
@@ -434,32 +436,51 @@ const resetGame = () => {
 }
 
 const startGame = () => {
-  const randomIndex = Math.floor(Math.random() * 51)
-
-  let x = deck[randomIndex].value
-
+  deck.sort((a, b) => 0.5 - Math.random())
+  
+  addPicP = () => {
+    var img = new Image()
+    img.src = deck[0].img
+    player.appendChild(img)
+  }
+  addPicP()
+  let x = deck[0].value
+  console.log(deck[0].value)
+ 
   cards.push(x)
 
   deck.shift()
   startGameButton.disabled = true
-  player.innerHTML = 'Cards: ' + ' ' + x.img
+
+
   total.innerHTML = 'Sum: ' + ' ' + cards
+  compareMessage.innerHTML = 'Your Sum:' + cards + ' ' + ' ; Dealer Sum:___'
 }
 
 const newCard = () => {
-  const randomIndex = Math.floor(Math.random() * 51)
 
-  let x = deck[randomIndex].value
-  let finalValue = []
-  cards.push(x)
-  sum = +cards
+  addPicP = () => {
+    var img = new Image()
+    img.src = deck[0].img
+    player.appendChild(img)
+  }
+  addPicP()
+  
+  let x = deck[0].value 
+  console.log(deck[0].value)
+  console.log(deck[0].img)
+
+ 
+  
+
   deck.shift()
   cards.forEach((num) => {
     finalValue.push(num)
     sum = finalValue.reduce((int, value) => int + value, 0)
 
-    player.innerHTML = 'Cards: ' + ' ' + cards
+    player.innerHTML = 'Cards:  ' + '  ' + cards
     total.innerHTML = 'Sum: ' + ' ' + sum
+    compareMessage.innerHTML = 'Your Sum:' + sum + ' ' + ' ; Dealer Sum:___'
 
     return finalValue
   })
